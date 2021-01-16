c21859783ac183e4775091d74712aa682585a5c2
nadaaeltayeb <nada.eltayeb.1999@gmail.com>
2021-01-16 19:27:23 +0200

    added python project

---
+++ svm_test/main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from sklearn.preprocessing import LabelEncoder
+from collections import defaultdict
+from nltk.corpus import wordnet as wn
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import model_selection, svm
+from sklearn.metrics import accuracy_score
+
+if __name__ == '__main__':
+    print('test')
+    #dataset = pd.read_csv(r"dataset.csv")
+
